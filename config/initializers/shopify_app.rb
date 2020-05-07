afdd2bf04ce680c5ba843f39d1cb0ab40d4aa41d
Erico <ericodias1@gmail.com>
2020-05-07 16:02:53 -0300

    scope fix

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "e094a22fa1ffa1dbf5a314a37402f74e"
   config.secret = "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"
   config.old_secret = "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"
-  config.scope = "wrire_shipping" # Consult this page for more scope options:
+  config.scope = "write_shipping" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
